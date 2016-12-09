90ad466648e68a67853d78af83ddbe457694c8e6
devFRIND <Bakury195@gmail.com>
2016-12-09 08:42:37 +0000

    Update hello.lua

--- plugins/hello.lua
+++ plugins/hello.lua
@@ -12,33 +12,29 @@ do
     
 local function run(msg,matches)
     if matches[1] == "chat_add_user"  then 
-     local text = ' اهلا بك في المجموعه كبد حياتي 🙈💋'..'\n'..'\n'
-    ..'🦁 اسمـ☆ـك :  '..msg.action.user.print_name..'\n'
-    ..'🦁 معـ☆ـرفك : @'..(msg.action.user.username or "لا يوجد")..'\n'
-    ..'🦁 الايـ❃ـدي : '..msg.action.user.id..'\n'
-    ..'🦁 رقـم هاتـ❅ـفك : '..(msg.action.user.phone or "لا يوجد")..'\n'
-    ..'🦁 الـي ضـ❉ـافك : '..msg.from.print_name..'\n'
-    ..'🦁 معـ☆ـرف الـي ضـافك : @'..(msg.from.username or "لا يوجد")..'\n'
+     local text = 'هــلا بل قالي 😍🍁'..'\n'..'\n'
+    ..'🎲 اسمـ☆ـك :  '..msg.action.user.print_name..'\n'
+    ..'🎲 معـ☆ـرفك : @'..(msg.action.user.username or "لا يوجد")..'\n'
+    ..'🎲 الـي ضـ❉ـافك : '..msg.from.print_name..'\n'
+    ..'🎲 معـ☆ـرف الـي ضـافك : @'..(msg.from.username or "لا يوجد")..'\n'
     ..'📆 الـتاريخ : '..os.date('!%A, %B %d, %Y*\n', timestamp)
     ..'🕧 الوقـت : '..os.date(' %T*', os.time())..'\n'  
      
      return reply_msg(msg.id, text, ok_cb, false)
      end
     if matches[1] == "chat_add_user_link" then
-      local text = ' اهلا بك في المجموعه كبد حياتي 🙈💋'..'\n'..'\n'
-     ..'🦁 اسـ☆ـمك :  '..msg.from.print_name..'\n'
-    ..'🦁 معـ☆ـرفك : @'..(msg.from.username or "لا يوجد")..'\n'
-    ..'🦁 الايـ❃ـدي : '..msg.from.id..'\n'
-    ..'🦁رقـم هاتـ❆ـفك : '..(msg.from.phone or "لا يوجد")..'\n'
-    ..'🦁 اسـم المجـ❆ـموعة : '..msg.to.title..'\n'
-    ..'🦁 ايـدي المجـ❈ـموعة : '..msg.to.id..'\n'
+      local text = ' هــلا بل قالي 😍🍁 '..'\n'..'\n'
+     ..'🎲 اسـ☆ـمك :  '..msg.from.print_name..'\n'
+    ..'🎲 معـ☆ـرفك : @'..(msg.from.username or "لا يوجد")..'\n'
+  ..'🎲 الـي ضـ❉ـافك : '..msg.from.print_name..'\n'
+    ..'🎲 معـ☆ـرف الـي ضـافك : @'..(msg.from.username or "لا يوجد")..'\n'
     ..'📆 الـتاريخ : '..os.date('!%A, %B %d, %Y*\n', timestamp)
     ..'🕧 الوقـت : '..os.date(' %T*', os.time())..'\n'  
      
         return reply_msg(msg.id, text, ok_cb, false)
   end
       if matches[1] == "chat_del_user" then
-       return " 🔸 الله وياك/ج كبد عمري 😍🙈 لتنسه الباب مفتوح 🌚"..msg.action.user.first_name
+       return " 🔸 الله وياك/ج انشالله هايش تنطحك بل طريق😂😂 🌚"..msg.action.user.first_name
 end
 end
  
