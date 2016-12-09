168ffb3d2b6f70fd66db7e9b93cf877fedb00500
devFRIND <Bakury195@gmail.com>
2016-12-09 10:27:15 +0000

    Update ar_version.lua

--- plugins/ar_version.lua
+++ plugins/ar_version.lua
@@ -12,7 +12,7 @@ do
 
 function run(msg, matches)
 local reply_id = msg['id']
-  local S = ' 👁‍🗨 سِـوَرَسِ KING TELE \n 🐯 الُاصّـ♔ـدِارَ :- الُثُالُثُ \n\n 🔭 الُمٌوَقًْع https://github.com/blackops97/KING_TELE \n\n ✈️ مٌطِوَرَيَنَ الُسِـوَرَسِ \n 🦁 بّـكِوَرَيَ :- @ll_B5 \n 🦁 صّـادِقً :- @sadikal_knani10 \n\n 🛰 قًنَاتْنَا ْعلُى الُتْلُيَ كِرَامٌ \n 👁‍🗨 @illOlli '
+  local S = ' 👁‍🗨 سِـوَرَسِ ‏‎♚ الۂمہٰٰاہٰٰـ(M.F.I)ـفيہه♚  \n 👁‍🗨 الُاصّـ♔ـدِارَ :- الُاول \n\n 🔭 الُمٌوَقًْع https://github.com/devFRIND/M.F.I_BOT \n\n ✈️ مٌطِوَرَيَنَ الُسِـوَرَسِ \n 👁‍🗨 بّـكِوَرَيَ :- @ll_B5 \n 👁‍🗨 صّـادِقً :- @sadikal_knani10 \n 👁‍🗨 السفاح :- @WA_WI \n\n 🛰 قًنَاتْنَا ْعلُى الُتْلُيَ كِرَامٌ \n 👁‍🗨 @illOlli '
 reply_msg(reply_id, S, ok_cb, false)
 end
 
