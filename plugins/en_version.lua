1a6c91b6177a7220801f1df3209aab29fca4ae84
devFRIND <Bakury195@gmail.com>
2016-12-09 08:59:56 +0000

    Update en_version.lua

--- plugins/en_version.lua
+++ plugins/en_version.lua
@@ -1,8 +1,8 @@
 --[[ 
 ▀▄ ▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀ 
 ▀▄ ▄▀                                      ▀▄ ▄▀
-       By ~ @sadikal-knani
-       @kingtele1 
+       By ~@ll_B5
+       @illOlli
 ▀▄ ▄▀                                      ▀▄ ▄▀ 
 ▀▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀ 
 --]] 
@@ -15,7 +15,7 @@ local chat = msg.to.id
     local oscar = 'mate:'..msg.to.id 
     if redis:get(oscar) and msg.media and not is_momod(msg) then 
     delete_msg(msg.id, ok_cb, false) 
-local test = "عزيزي👿~["..msg.from.first_name.."]".."\n".."اه ولك/ج هم قافلينهن وهم ما سالمين منك/ج ولك/ج ممنوع يعني ممنوع اذا انعادت اسمط اهل اهلك/ج{❌}👿".."\n".."👾username: @"..(msg.from.username or " ") 
+local test = "حُبّۂ الُوَسِائطِۂ مٌقًفَوَلُُُﮥ لُا تْرَسِلۂُ انَوَبۂّ حُتْﯽ مٌاطِرَدِكِۂ'..'\n🎓مٌْعرَفَكِۂ @"..(msg.from.username or " ") 
 reply_msg(jalal, test, ok_cb, true) 
 
 end 
@@ -29,7 +29,7 @@ local jalal = msg['id']
     if matches[1] == 'قفل الوسائط'  and is_momod(msg) then 
                     local oscar = 'mate:'..msg.to.id 
                     redis:set(oscar, true) 
-                    local oscar1 = 'تــم قفــل 🔐✅الوســائط .. \n'..'\n\n'..msg.to.title..'\n🛠MSG BY :-@'..msg.from.username..'\n'
+                    local oscar1 = 'تــم قفــل 🔐✅الوســائط .. \n'..'\n\n'..'🛰MSG BY :-@'..msg.from.username..'\n'
 reply_msg(jalal, oscar1, ok_cb, true) 
 elseif matches[1] == 'قفل الوسائط' and not is_momod(msg) then 
 local asdy = 'للمشرفين فقط🔴' 
@@ -38,7 +38,7 @@ reply_msg(jalal, asdy, ok_cb, true)
     elseif matches[1] == 'فتح الوسائط'  and is_momod(msg) then 
       local oscar = 'mate:'..msg.to.id 
       redis:del(oscar) 
-    local don = 'تـ﷽ـمٌ فَتْحُ جْمٌيَْع الُوَسِائطِ\n'..'\n\n'..msg.to.title..'\n🛠MSG BY :-@'..msg.from.username..'\n'
+    local don = 'تـمٌ فَتْحُ جْمٌيَْع✅🔱 الُوَسِائطِ\n'..'\n\n'..'🛰MSG BY :-@'..msg.from.username..'\n'
 reply_msg(jalal, don, ok_cb, true) 
 elseif matches[1] == 'قفل الوسائط' and not is_momod(msg) then 
 local jalal_aldon = 'للمشرفين فقط🔴' 
@@ -55,4 +55,4 @@ run = run,
     pre_process = pre_process 
 } 
 
-end 
+end
